6731de9f80f5bb5c07fe5bf3fe545941b67408c7
Valerio Di Felice <valesmile.99@gmail.com>
2025-10-23 21:00:02 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# HexTec-Hub
+
---
+++ backend/src/test/java/com/example/hextec/hub/HextecHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hextec.hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HextecHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
