0288a16c354b06f95bce6b92117e3e1df1b0dac6
Valerio Di Felice <valesmile.99@gmail.com>
2025-11-07 04:23:54 +0100

    Add AuthFilter and configure JWT dependencies
    Introduced AuthFilter for request authentication and updated pom.xml to include JWT dependencies. Also added PostgreSQL and JPA configuration to application.properties for database connectivity and Hibernate settings.

---
+++ Backend/src/main/java/com/example/hextec/hub/auth/AuthFilter.java
@@ -0,0 +1,22 @@
+package com.example.hextec.hub.auth;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class AuthFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authToken = request.getHeader("Authorization");
+        if (authToken == null || !isValidToken(authToken)) {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+            return; 
+    }
+}
